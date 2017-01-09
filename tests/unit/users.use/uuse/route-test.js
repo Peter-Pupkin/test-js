f10e87b765b81ef4f292a26e843f94b2909fd9ee
Peter-Pupkin <na_lu2004@mail.ru>
2017-01-09 18:03:50 +0300

    branch6

---
+++ tests/unit/users.use/uuse/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:users.use/uuse', 'Unit | Route | users.use/uuse', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
