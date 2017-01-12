b932d4f64e54a8b00c2f99a2047ab04ded6382fb
Peter-Pupkin <na_lu2004@mail.ru>
2017-01-12 17:44:04 +0300

    branch5

--- app/posts/route.js
+++ app/posts/route.js
@@ -2,7 +2,48 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     
-     model(){
-        return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/');
+    //  model(){
+    //    return Ember.$.when(
+    //         Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/'),
+    //         Ember.$.getJSON('https://jsonplaceholder.typicode.com/users')
+    //         )
+    //         .then( ([allposts], [allusers]) => {
+    //             allusers = Ember.A(allusers);//????
+    //             allposts.forEach((ppp) => ppp.uuu = allusers.findBy('id', ppp.userId));
+    //             return allposts;  
+    //         })            
+                   
+    //         .then( (allposts) => {
+    //                return allposts.filter((item, index) => index < 20);
+
+    //         }); 
+
+    model() {
+        return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts')
+            .then((posts) => {
+                // http://emberjs.com/api/classes/Ember.Array.html
+                posts = Ember.A(posts);
+                // let userIds = posts.map((post) => post.userId);
+                let userIds = posts.mapBy('userId');
+
+                // let uniqUserIds = Object.values(userIds.reduce((hash, id) => {
+                //     hash[id] = id;
+                //     return hash;
+                // }, {}));
+                let uniqUserIds = Ember.A(userIds).uniq();
+
+                let usersList = uniqUserIds.map((id) => Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`));
+                return Ember.$.when(...usersList).then(function() {
+                
+                    let data = [].slice.call(arguments);
+                    const users = Ember.A(data).mapBy('firstObject');
+                    
+                    posts.forEach((post) => post.user = users.findBy('id', post.userId));
+
+                    return posts;
+                });
+            });
+
+
     }
 });
