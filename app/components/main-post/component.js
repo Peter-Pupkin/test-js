1fc22da735cf8a4c8c5a9acb24b69db9dabc04a3
Peter-Pupkin <na_lu2004@mail.ru>
2017-02-09 12:50:09 +0300

    branch13

--- app/components/main-post/component.js
+++ app/components/main-post/component.js
@@ -1,31 +1,87 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-     posts:null,
-     activePost1: null,
-     activePost2: null,
-     actions: {
-        setBodyView (post) {
+    posts: null,
+    activePost1: null,
+    activePost2: null,
+    actions: {
+        setBodyView(p) {
 
-            let activePost1 = this.get ('activePost1');
-            let activePost2 = this.get ('activePost2');
-            if (activePost1  && activePost2 === post){
-             this.set('activePost1', post);
-             this.set('activePost2', post);
-            }else{
-             this.set('activePost1', null);        
-             this.set('activePost2', null); }
+            let a1 = this.get('activePost1');
+            let a2 = this.get('activePost2');
+            if (!a1 && !a2) {
+                this.set('activePost1', p);
+            } else if (a1 && !a2) {
 
-             if (activePost1 === activePost2){
+                if (a1 == p) {
                     this.set('activePost1', null);
+                }
+                else {
+                    this.set('activePost2', p);
+                }
+            }
+            else if (a1 && a2) {
+                if (a1 == p) {
+                    this.set('activePost1', null);
+                } else {
+                    this.set('activePost1', p);
+                }
+                if (a2 == p) {
+                    this.set('activePost2', null);
+                } else {
                     this.set('activePost2', post);
-                    }else{
-                        this.set('activePost2', null);
-                        this.set('activePost1', post);
-                    }
-            
-           
-        }
+                }
 
+
+
+
+            }
+            else if (!a1 && a2) {
+                if (a2 == p) {
+                    this.set('activePost2', null);
+                } else {
+                    this.set('activePost1', post);
+                }
+            }
+
+            else if (a1 && !a2) {
+                if (a1 == p) {
+                    this.set('activePost1', null);
+                } else {
+                    this.set('activePost2', post);
+                }
+            }
+
+        }
     }
+
+}
+
+    // if (activePost1 !== post && activePost2 !== post) {
+    //     this.set('activePost1', post);
+    // }
+
+    // if (activePost1 === post) {
+    //     this.set('activePost1', null);
+    // } else {
+    //     this.set('activePost2', post);
+    // }
+
+
+
+    // if (activePost1 === post && activePost2 === post) {
+    //     if (activePost1 === post) {
+    //         this.set('activePost1', null);
+    //     } else {
+    //         if (activePost2 === post) {
+    //             this.set('activePost2', null);
+    //         }
+    //     }
+    //     if (activePost2 === post) {
+    //         this.set('activePost2', null);
+    //     } else {
+    //         this.set('activePost1', post);
+    //     }
+
+    // }
 });
--- app/posts/route.js
+++ app/posts/route.js
@@ -2,47 +2,47 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     
-    //  model(){
-    //    return Ember.$.when(
-    //         Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/'),
-    //         Ember.$.getJSON('https://jsonplaceholder.typicode.com/users')
-    //         )
-    //         .then( ([allposts], [allusers]) => {
-    //             allusers = Ember.A(allusers);//????
-    //             allposts.forEach((ppp) => ppp.uuu = allusers.findBy('id', ppp.userId));
-    //             return allposts;  
-    //         })            
+     model(){
+       return Ember.$.when(
+            Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts/'),
+            Ember.$.getJSON('https://jsonplaceholder.typicode.com/users')
+            )
+            .then( ([allposts], [allusers]) => {
+                allusers = Ember.A(allusers);//????
+                allposts.forEach((ppp) => ppp.uuu = allusers.findBy('id', ppp.userId));
+                return allposts;  
+            })            
                    
-    //         .then( (allposts) => {
-    //                return allposts.filter((item, index) => index < 20);
+            .then( (allposts) => {
+                   return allposts.filter((item, index) => index < 20);
+            
+            }); 
 
-    //         }); 
+    // model() {
+    //     return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts')
+    //         .then((posts) => {
+    //             // http://emberjs.com/api/classes/Ember.Array.html
+    //             posts = Ember.A(posts);
+    //             // let userIds = posts.map((post) => post.userId);
+    //             let userIds = posts.mapBy('userId');
 
-    model() {
-        return Ember.$.getJSON('https://jsonplaceholder.typicode.com/posts')
-            .then((posts) => {
-                // http://emberjs.com/api/classes/Ember.Array.html
-                posts = Ember.A(posts);
-                // let userIds = posts.map((post) => post.userId);
-                let userIds = posts.mapBy('userId');
+    //             // let uniqUserIds = Object.values(userIds.reduce((hash, id) => {
+    //             //     hash[id] = id;
+    //             //     return hash;
+    //             // }, {}));
+    //             let uniqUserIds = Ember.A(userIds).uniq();
 
-                // let uniqUserIds = Object.values(userIds.reduce((hash, id) => {
-                //     hash[id] = id;
-                //     return hash;
-                // }, {}));
-                let uniqUserIds = Ember.A(userIds).uniq();
-
-                let usersList = uniqUserIds.map((id) => Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`));
-                return Ember.$.when(...usersList).then(function() {
+    //             let usersList = uniqUserIds.map((id) => Ember.$.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`));
+    //             return Ember.$.when(...usersList).then(function() {
                 
-                    let data = [].slice.call(arguments);
-                    const users = Ember.A(data).mapBy('firstObject');
+    //                 let data = [].slice.call(arguments);
+    //                 const users = Ember.A(data).mapBy('firstObject');
                     
-                    posts.forEach((post) => post.user = users.findBy('id', post.userId));
+    //                 posts.forEach((post) => post.user = users.findBy('id', post.userId));
 
-                    return posts;
-                });
-            });
+    //                 return posts;
+    //             });
+    //         });
 
 
     }
